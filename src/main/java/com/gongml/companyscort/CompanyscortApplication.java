145c935e2cfe9bc17e99b46108e021f3a090ce0b
gongml <1019753410@qq.com>
2018-12-27 17:54:29 +0800

    111

--- src/main/java/com/gongml/companyscort/CompanyscortApplication.java
+++ src/main/java/com/gongml/companyscort/CompanyscortApplication.java
@@ -1,19 +1,13 @@
 package com.gongml.companyscort;
 
-import com.gongml.companyscort.schedule.CollectionInfoScheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CompanyscortApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(CompanyscortApplication.class, args);
-        CollectionInfoScheduler bean = context.getBean(CollectionInfoScheduler.class);
-//        bean.collectionCompanyInfos();
-        bean.collectionCompanyNewss();
-        context.close();
+        SpringApplication.run(CompanyscortApplication.class, args);
     }
 
 }
